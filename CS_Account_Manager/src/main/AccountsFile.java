d038e954f800ceeec67067b123db3ad2e9b3e573
Auston Rogers <austonpowers@live.com>
2017-10-26 10:49:37 -0700

    Signed-off-by: Auston Rogers <austonpowers@live.com>

--- CS_Account_Manager/src/main/AccountsFile.java
+++ CS_Account_Manager/src/main/AccountsFile.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 public class AccountsFile {
  public ArrayList<AccountMember> AccountMembers;
 
+ public AccountsFile() {
+  AccountMembers = new ArrayList<AccountMember>();
+ }
+
  public void addToArrayList(AccountMember member) {
   AccountMembers.add(member);
  }
