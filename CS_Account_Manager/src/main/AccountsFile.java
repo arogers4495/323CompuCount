3da51fd57315710c5da3d4cbcae33c1c3d9e787e
Auston Rogers <austonpowers@live.com>
2017-10-26 10:43:36 -0700

    Added AccountsFile class

---
+++ CS_Account_Manager/src/main/AccountsFile.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class AccountsFile {
+ public ArrayList<AccountMember> AccountMembers;
+
+ public void addToArrayList(AccountMember member) {
+  AccountMembers.add(member);
+ }
+}
